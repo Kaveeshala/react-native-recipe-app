794be066069f69dc611d4c7ef7a3b0aeb7f58949
kaveeshala <kaveeshala2003@gmail.com>
2026-03-12 19:25:51 +0530

    refactor layout and index components for improved structure and navigation

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
 import './global.css';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView className="flex flex-1">
+       <Stack screenOptions={{ headerShown: false }} />
+       <StatusBar />
+    </SafeAreaView>
+  )
 }
