ff1231d00ffb80e5824ebd38d54be38db20769df
kaveeshala <kaveeshala2003@gmail.com>
2026-03-13 13:39:08 +0530

    create recipe model

---
+++ backend/src/models/recipe.model.js
@@ -0,0 +1,20 @@
+import pool from "../config/db.js";
+
+const createRecipe = async (title, description, category, cooking_time, image_url, user_id) => {
+  const result = await pool.query(
+    `INSERT INTO recipes (title, description, category, cooking_time, image_url, user_id)
+     VALUES ($1, $2, $3, $4, $5, $6)
+     RETURNING *`,
+    [title, description, category, cooking_time, image_url, user_id]
+  );
+  return result.rows[0];
+};
+
+const getAllRecipes = async () => {
+  const result = await pool.query(
+    "SELECT * FROM recipes ORDER BY created_at DESC"
+  );
+  return result.rows;
+};
+
+export { createRecipe, getAllRecipes };
