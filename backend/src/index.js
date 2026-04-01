82e9aab50340dac627c7ca9c54ead81ad070c648
kaveeshala <kaveeshala2003@gmail.com>
2026-04-01 13:40:18 +0530

    change port number

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,6 +46,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
